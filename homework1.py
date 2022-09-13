b42d46d0791fdbdc06d10408616c10cc9585ad50
LubinNK <lubin.nk@phystech.edu>
2022-09-13 15:48:47 +0300

    Second commit

--- homework1.py
+++ homework1.py
@@ -1,4 +1,5 @@
 def step1():
+    """ Starting step for duck """
     print(
         'Утка-маляр 🦆 решила выпить зайти в бар. '
         'Взять ей зонтик? ☂️'
@@ -8,18 +9,20 @@ def step1():
     while option not in options:
         print('Выберите: {}/{}'.format(*options))
         option = input()
-    
     if options[option]:
         return step2_umbrella()
     return step2_no_umbrella()
 
+
 def step2_umbrella():
+    """Second step for duck with umbrella"""
     print(
         'Утка приняла зонтик! \n' 
         'Теперь она не промокнет, если пойдет дождь :)'
     )
 
 def step2_no_umbrella():
+    """Second step for duck without umbrella"""
     print('Осторожно, утка может промокнуть!')
 
 if __name__ == '__main__':

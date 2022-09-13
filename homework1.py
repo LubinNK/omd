ca2ed55e7ce1358959007749e523f2be492c7502
LubinNK <lubin.nk@phystech.edu>
2022-09-13 16:09:12 +0300

    Change the code for PEP8

--- homework1.py
+++ homework1.py
@@ -1,29 +1,59 @@
-def step1():
-    """ Starting step for duck """
-    print(
-        'Утка-маляр 🦆 решила выпить зайти в бар. '
-        'Взять ей зонтик? ☂️'
-    )
+def get_user_answer():
     option = ''
     options = {'да': True, 'нет': False}
     while option not in options:
         print('Выберите: {}/{}'.format(*options))
         option = input()
-    if options[option]:
-        return step2_umbrella()
-    return step2_no_umbrella()
+    return options[option]
+
+
+def step1():
+    print(
+        'Утка-маляр 🦆 решила выпить зайти в бар. '
+        'Взять ей зонтик? ☂️'
+    )
+    user_answer = get_user_answer()
+    if user_answer:
+        step2_umbrella()
+    else:
+        step2_no_umbrella()
 
 
 def step2_umbrella():
-    """Second step for duck with umbrella"""
     print(
-        'Утка приняла зонтик! \n' 
-        'Теперь она не промокнет, если пойдет дождь :)'
+        'Утка приняла зонтик!\n' 
+        'Теперь она не промокнет, если пойдет дождь :)\n'
+        'Утка добралась до места, взять ей пиво?'
     )
+    user_answer = get_user_answer()
+    if user_answer:
+        step3_beer()
+    else:
+        step3_no_beer()
+
 
 def step2_no_umbrella():
-    """Second step for duck without umbrella"""
-    print('Осторожно, утка может промокнуть!')
+    print(
+        'Утке повезло, дождя не было, она дошла до бара!\n'
+        'Она просит пиво, вы ей разрешаете?'
+    )
+    user_answer = get_user_answer()
+    if user_answer:
+        step3_beer()
+    else:
+        step3_no_beer()
+
+
+def step3_beer():
+    print('Утка довольна, она любит Немецкое Нефильтрованное ...')
+
+
+def step3_no_beer():
+    print(
+        'Утка расстроилась :(\n'
+        'Она недовольна и ушла домой ...'
+    )
+
 
 if __name__ == '__main__':
     step1()

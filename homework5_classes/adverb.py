2fbbf3f43dd685b2c072acdc07f4ad748df56a43
LubinNK <lubin.nk@phystech.edu>
2022-11-02 13:22:02 +0300

    Исправления после сдачи

--- homework5_classes/adverb.py
+++ homework5_classes/adverb.py
@@ -1,5 +1,6 @@
 """ Working with classes and JSON objects """
 import json
+from keyword import iskeyword
 
 
 class JSONReader:
@@ -7,9 +8,16 @@ class JSONReader:
     def __init__(self, dict_json: dict):
         for attr, val_attr in dict_json.items():
             if isinstance(val_attr, dict):
-                setattr(self, attr, JSONReader(val_attr))
+                setattr(self, self.is_key(attr), JSONReader(val_attr))
             else:
-                setattr(self, attr, val_attr)
+                setattr(self, self.is_key(attr), val_attr)
+
+    @staticmethod
+    def is_key(word: str):
+        """Check if word is keyword, and change it if yes"""
+        if iskeyword(word) or word == 'price':
+            word = ''.join(list(word)+['_'])
+        return word
 
 
 class ColorizeMixin:
@@ -18,27 +26,49 @@ class ColorizeMixin:
         return f'\033[{self.repr_color_code}m {self.title} | {self.price} $'
 
 
-class Advert(ColorizeMixin, JSONReader):
+class BaseAdvert(JSONReader):
     """For adverb"""
-    repr_color_code = 32
 
     def __init__(self, adv: dict):
-        if 'price' in adv and adv['price'] >= 0:
-            super().__init__(adv)
-        elif 'price' in adv:
-            raise ValueError('price must be >= 0')
-        else:
-            self.price = 0
-            super().__init__(adv)
+        self._norm_title = True
+        self._norm_price = True
+        if 'title' not in adv:
+            self._norm_title = False
+        if 'price' not in adv:
+            self.price_ = 0
+        elif adv['price'] < 0:
+            self._norm_price = False
+        if not self._norm_title:
+            raise ValueError('Title is the necessary attribute!')
+        if not self._norm_price:
+            raise ValueError('Price must be >= 0.')
+        super().__init__(adv)
+
+    @property
+    def price(self):
+        """Price property"""
+        return self.price_
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price must be >= 0")
+        self.price_ = value
 
     def __repr__(self):
-        return super().__repr__()
+        return f'{self.title} | {self.price} $'
+
+
+class Advert(ColorizeMixin, BaseAdvert):
+    """Main Class Advert"""
+    repr_color_code = 32
 
 
 def main():
     """Main Launch"""
     lesson_str = """{
         "title": "python",
+        "class": "lesson",
         "location": {
             "address": "город Москва, Лесная, 7",
             "metro_stations": ["Белорусская"]
@@ -48,6 +78,7 @@ def main():
     lesson_ad = Advert(lesson)
     print(lesson_ad.location.address)
     print(lesson_ad.price)
+    print(lesson_ad.class_)
     print(lesson_ad)
 
 

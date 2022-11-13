40e90f3aa5e6a089dcce1fb2e78b4d9e3956f323
LubinNK <lubin.nk@phystech.edu>
2022-11-13 20:06:41 +0300

    Add test_issue_05.py

---
+++ homework6_testing/test_issue_05.py
@@ -0,0 +1,26 @@
+"""Testing what_is_year_now function"""
+import pytest
+from unittest.mock import patch
+from what_is_year_now import what_is_year_now
+
+
+def test_year1():
+    """Test 1"""
+    with patch('json.load') as mocked_get_cases:
+        mocked_get_cases.return_value = {'currentDateTime': '2022-11-13'}
+        assert what_is_year_now() == 2022
+
+
+def test_year2():
+    """Test 2"""
+    with patch('json.load') as mocked_get_cases:
+        mocked_get_cases.return_value = {'currentDateTime': '13.11.2022'}
+        assert what_is_year_now() == 2022
+
+
+def test_year3():
+    """Test 3"""
+    with patch('json.load') as mocked_get_cases:
+        mocked_get_cases.return_value = {'currentDateTime': '13-11-2022'}
+        with pytest.raises(ValueError):
+            what_is_year_now()

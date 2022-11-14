569a1ffccc5d2e944369b5e38010cfb91c418cd1
LubinNK <lubin.nk@phystech.edu>
2022-11-14 15:26:48 +0300

    Update issue_05 with mock

--- homework6_testing/test_issue_05.py
+++ homework6_testing/test_issue_05.py
@@ -1,34 +1,36 @@
 """Testing what_is_year_now function"""
-import pytest
+import urllib.request
+import io
 from unittest.mock import patch
+import pytest
 from what_is_year_now import what_is_year_now
 
 
 def test_year1():
     """Test 1"""
-    with patch('json.load') as mocked_get_cases:
-        mocked_get_cases.return_value = {'currentDateTime': '2022-11-13'}
+    data = io.StringIO('{\'currentDateTime\': \'2022-11-13\'}')
+    with patch.object(urllib.request, 'urlopen', return_value=data):
         assert what_is_year_now() == 2022
 
 
 def test_year2():
     """Test 2"""
-    with patch('json.load') as mocked_get_cases:
-        mocked_get_cases.return_value = {'currentDateTime': '13.11.2022'}
+    data = io.StringIO('{\'currentDateTime\': \'13.11.2022\'}')
+    with patch.object(urllib.request, 'urlopen', return_value=data):
         assert what_is_year_now() == 2022
 
 
 def test_year3():
     """Test 3"""
-    with patch('json.load') as mocked_get_cases:
-        mocked_get_cases.return_value = {'currentDateTime': '13-11-2022'}
+    data = io.StringIO('{\'currentDateTime\': \'13-11-2022\'}')
+    with patch.object(urllib.request, 'urlopen', return_value=data):
         with pytest.raises(ValueError):
             what_is_year_now()
 
 
 def test_year4():
     """Test 4"""
-    with patch('json.load') as mocked_get_cases:
-        mocked_get_cases.return_value = {'DateTime': '13-11-2022'}
+    data = io.StringIO('{\'DateTime\': \'13-11-2022\'}')
+    with patch.object(urllib.request, 'urlopen', return_value=data):
         with pytest.raises(KeyError):
             what_is_year_now()
